a2df899fd04542eb9d0f61880ffd83601087efb1
XaraKat <71393080+XaraKat@users.noreply.github.com>
2021-06-02 12:21:40 +0300

    lo4js
    log

--- index.js
+++ index.js
@@ -7,15 +7,36 @@ var log4js = require("log4js");
 
 app.use(cors())
 app.use(express.json());
+log4js.configure({
+    appenders: {
+      DefaultLogs: { type: 'file', filename: 'savedLogs.log' },
+      Keeper: { type: 'datefile', filename: 'sLogs.log' ,pattern:'.yyyy-MM-dd-hh'},
+      Errors: { type: 'file', filename: 'errors.log' },
+      console: { type: 'console' }
+    },
+    categories: {
+      app: { appenders: ['Errors'], level: 'error' },
+      keep: { appenders: ['console','Keeper'], level: 'debug' },
+      another: { appenders: ['console'], level: 'trace' },
+      default: { appenders: ['console', 'DefaultLogs'], level: 'trace' }
+    }
+  });
 
-
+const errLogger = log4js.getLogger('app')
+const defLogger = log4js.getLogger('keep')
 // app.listen('8080')
-app.listen(port, () => console.log(`listening on port: ${port}`))
+app.listen(port, () => defLogger.debug(`listening on port: ${port}`))
 
 app.get('/',function(req,res){
-    db.select().from('info').then(function(data){
-        res.send(data)
-    })
+    try{
+        db.select().from('info').then(function(data){
+            res.send(data)
+        })
+    }catch(e){
+        errLogger.error(e)
+        errLogger.error("Error sending data to db")
+    }
+   
     
 })
 
@@ -23,7 +44,7 @@ app.get('/',function(req,res){
 app.post('/', function(req, res) {
      
      db.insert(req.body).into('info').then(function(data){
-      console.log(req.body)
+      defLogger.info(req.body)
       res.sendStatus(200)
     })
 });
